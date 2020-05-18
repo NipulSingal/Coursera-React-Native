34b02cd2e14950b084b4bccd01b2286830599e2d
Nipul Singal <nipul00rock@gmail.com>
2020-05-18 14:14:27 +0530

    week 1 exercises

--- component/Main.js
+++ component/Main.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Menu from './Menu';
 import Dishdetail from './Dishdetail';
 import { View, Platform } from 'react-native';
-// import { DISHES } from '../shared/dishes';
 import Home from './Home'
 import { createStackNavigator , createDrawerNavigator} from 'react-navigation';
 
@@ -58,21 +57,11 @@ const MainNavigator=createDrawerNavigator({
     drawerBackgroundColor:'#D1C4E9'
 })
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      };
-  }
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
-    }
   render() {
  
     return (
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MainNavigator />
+            <MainNavigator/>
         </View>   );
   }
 }
